a50ebe08d7bc35651e02afad21b735fae216d82f
Julien Antony <coupdebol@gmail.com>
2015-06-16 19:40:21 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# LoginSiteNode
+A simple template
+
+# Installation
+`npm install -g nodemon` to install the command to start the project
+
+`npm install` should install all the required dependencies
+
+`nodemon app.js` will start the server
+
+`localhost:3000` to visit the site
---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+var stormpathMiddleware = stormpath.init(app, {
+  apiKeyFile: '/Users/Debol/Documents/testProjectToErase/apiKey.properties',
+  application: 'https://api.stormpath.com/v1/applications/2gPF3bYKY94tEoYMIpmMPL',
+  secretKey: 'some_long_random_string',
+  expandCustomData: true,
+  enableForgotPassword: true
+});
+
+app.use(stormpathMiddleware);
+
+app.get('/', function(req, res) {
+  res.render('home', {
+    title: 'Welcome'
+  });
+});
+
+app.use('/profile',stormpath.loginRequired,require('./profile')());
+app.listen(3000);
+
+
+/*
+
+https://stormpath.com/blog/build-nodejs-express-stormpath-app/
+
+
+*/
